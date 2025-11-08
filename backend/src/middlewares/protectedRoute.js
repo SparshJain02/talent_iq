9d4ffbef5705db40a1ddb2ab9d4991d1d0f322a5
Sparsh <codewithsparshjain@gmail.com>
2025-11-08 11:31:16 +0530

    feat: integrate Clerk for user authentication and add chat token endpoint

---
+++ backend/src/middlewares/protectedRoute.js
@@ -0,0 +1,24 @@
+import {requireAuth ,} from "@clerk/express";
+import { User } from "../models/User.js";
+
+
+export const protectRoute = [
+    requireAuth({signInUrl: "/signup"}), // this will redirect user authentication
+    async(req,res,next)=>{
+        // let me find whether user exists in db or not
+        const clerkId = req.auth.userId;
+        try{
+            const user = await User.findOne({clerkId})
+            if(!res){
+                return res.status(401).json({message:"user does not exist"})
+            }
+            // if user exist and in db then: 
+            req.user = user;
+            next();
+        }
+        catch(err){
+          console.log("Error in protected Route middleware",err);
+          res.status(500).json({message: "user authentication failed!"});
+        }
+    }
+]
