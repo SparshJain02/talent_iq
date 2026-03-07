d7cc43d074b3e64ed059ef1b7e96b924cf0f9b77
Sparsh <codewithsparshjain@gmail.com>
2026-03-07 19:33:06 +0530

    added debug

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import { ENV } from "./lib/env.js";
 import { connectDb } from "./lib/db.js";
 import {serve} from "inngest/express"
 import { inngest,functions } from "./lib/inngest.js";
-import { clerkMiddleware } from '@clerk/express' // adds req.auth.userId
+import { clerkMiddleware, requireAuth } from '@clerk/express' // adds req.auth.userId
 import chatRoutes from "./routes/chatRoutes.js";
 import sessionRoutes from "./routes/sessionRoutes.js";
 import cors from "cors"
@@ -23,13 +23,18 @@ app.use(cors({
 app.use("/api/chat",chatRoutes);
 app.use("/api/sessions",sessionRoutes);
 
+app.use("/debug",(req,res)=>{
+    console.log(req.headers.cookie);
+    console.log(req.headers.authorization);
+    res.json({ cookies: req.headers.cookie, auth: req.headers.authorization });
+})
 
-await connectDb();
-export default app;
-// app.listen(ENV.PORT, async () => {
-//     await connectDb();
-//     console.log("server is running on port" , ENV.PORT)
-// })
+// await connectDb();
+// export default app;
+app.listen(ENV.PORT, async () => {
+    await connectDb();
+    console.log("server is running on port" , ENV.PORT)
+})
 
 // // make our app ready for deployment
 // if (ENV.NODE_ENV === "production") {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,17 +1,6 @@
-import axios from "axios";
-import { getToken } from "@clerk/clerk-react";
-
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
-  withCredentials: true,
-});
-
-axiosInstance.interceptors.request.use(async (config) => {
-  const token = await getToken();
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-export default axiosInstance
+import axios from "axios"
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true
+})
+export default axiosInstance;
